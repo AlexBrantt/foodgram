ee338080e8ba55410ffa1ea158def38987635a8e
Алекс Брант <cantstop923@ya.ru>
2024-12-11 19:26:26 +0200

    wf

--- backend/api/admin.py
+++ backend/api/admin.py
@@ -1,6 +1,40 @@
 from django.contrib import admin
 
-from api.models import Ingredient, Recipe, RecipeIngredient, Tag
+from api.models import (
+    FavoriteRecipe,
+    Ingredient,
+    Recipe,
+    RecipeIngredient,
+    ShoppingList,
+    Subscription,
+    Tag,
+)
+
+
+@admin.register(Subscription)
+class SubscriptionAdmin(admin.ModelAdmin):
+    list_display = ("user", "author")
+    search_fields = ("user__username", "author__username")
+    list_filter = ("user", "author")
+
+
+@admin.register(ShoppingList)
+class ShoppingListAdmin(admin.ModelAdmin):
+    list_display = ("user", "recipe")
+    search_fields = ("user__username", "recipe__name")
+    list_filter = ("user",)
+
+
+@admin.register(FavoriteRecipe)
+class FavoriteRecipeAdmin(admin.ModelAdmin):
+    list_display = ("user", "recipe", "recipe_author")
+    search_fields = ("user__username", "recipe__name")
+    list_filter = ("user",)
+
+    def recipe_author(self, obj):
+        return obj.recipe.author.username
+
+    recipe_author.short_description = 'Автор рецепта'
 
 
 @admin.register(Ingredient)
